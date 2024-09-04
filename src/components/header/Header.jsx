0cec5369cd3d4234138728f8cdcce6357a59a96c
KiwiScreams <screamskiwi3@gmail.com>
2024-09-04 13:04:55 +0400

    .

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   const [isScrolling, setIsScrolling] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 50) {
+      if (window.scrollY > 100) {
         setIsScrolling(true);
       } else {
         setIsScrolling(false);
--- src/components/info container/InfoContainer.jsx
+++ src/components/info container/InfoContainer.jsx
@@ -1,6 +1,8 @@
 import "./InfoContainer.css";
 import imageTransform from "../../assets/desktop/image-transform.jpg";
 import imageStandOut from "../../assets/desktop/image-stand-out.jpg";
+import imageGraphicDesignder from "../../assets/desktop/image-graphic-design.jpg";
+import imagePhotography from "../../assets/desktop/image-photography.jpg";
 const InfoContainer = () => {
   return (
     <>
@@ -31,6 +33,39 @@ const InfoContainer = () => {
               that do most of the marketing for you.
             </p>
             <h3 className="learn-2">Learn More</h3>
+          </div>
+        </div>
+        <div className="container">
+          <div
+            className="box"
+            style={{
+              backgroundImage: `url(${imageGraphicDesignder})`,
+              backgroundSize: "100% 100%",
+            }}
+          >
+            <div>
+              <h2>Graphic Design</h2>
+              <p>
+                Great design makes you memorable. We deliver artwork that
+                underscores your brand message and captures potential clients'
+                attention.
+              </p>
+            </div>
+          </div>
+          <div
+            className="box"
+            style={{
+              backgroundImage: `url(${imagePhotography})`,
+              backgroundSize: "100% 100%",
+            }}
+          >
+            <div>
+              <h2>Photography</h2>
+              <p>
+                Increase your credibility by getting the most stunning,
+                high-quality photos that improve your business image.
+              </p>
+            </div>
           </div>
         </div>
       </section>
