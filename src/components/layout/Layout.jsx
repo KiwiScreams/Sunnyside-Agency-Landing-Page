b712340545c0761f43d4c916ed75d33bdf49ce77
KiwiScreams <screamskiwi3@gmail.com>
2024-09-04 10:59:38 +0400

    layout component style

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,9 +1,11 @@
 import "./Layout.css";
+import arrow from "../../assets/images/icon-arrow-down.svg";
 const Layout = () => {
     return ( 
         <>
         <section className="layout-section">
             <h1>WE ARE CREATIVES</h1>
+            <img src={arrow} alt="" />
         </section>
         </>
      );
